15db1a32af3dec2dc7db92419269a7112244e6d8
RUSTENT_SVERD <skogensvoktere@hotmail.com>
2023-11-29 11:26:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PP_Oppgave_UKE_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
